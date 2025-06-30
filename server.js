ac0ad8ab35873a680ea19a56d165b52a972deb43
Kawser420 <muhammadkawser202@gmail.com>
2025-06-30 22:17:49 +0600

    add todo json file created now

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "./db/todo.json");
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method);
-  // res.end("Welcome to basic todo app server");
-
+  // GET All ToDos
   if (req.url === "/todos" && req.method === "GET") {
-    res.end("All ToDos");
-  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
-    res.end("ToDo Created");
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+
+    res.end(data);
+  }
+  // POST a Todo
+  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+    req.on("end", () => {
+      const { id, title, description, completed, priority, tags } =
+        JSON.parse(data);
+
+      const dueDate = new Date().toLocaleString();
+      const allToDos = fs.readFileSync(filePath, { encoding: "utf-8" });
+      const parseAllTodos = JSON.parse(allToDos);
+      console.log(allToDos);
+
+      parseAllTodos.push({
+        id,
+        title,
+        description,
+        completed,
+        dueDate,
+        priority,
+        tags,
+      });
+
+      fs.writeFileSync(filePath, JSON.stringify(parseAllTodos, null, 2), {
+        encoding: "utf-8",
+      });
+      res.end(
+        JSON.stringify(
+          {
+            id,
+            title,
+            description,
+            completed,
+            dueDate,
+            priority,
+            tags,
+          },
+          null,
+          2
+        )
+      );
+    });
   } else {
-    res.end("404------>Route Not Found!!!!!!!!");
+    res.end("Route Not Found!");
   }
 });
 
