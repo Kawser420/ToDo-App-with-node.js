0d7f41f7c7bdc8c47f97d713e68e207364184a77
Kawser420 <muhammadkawser202@gmail.com>
2025-06-30 19:38:40 +0600

    fix: routing nodejs and GET, CREATED ToDos

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Welcome to basic todo app server");
+  // console.log(req.url, req.method);
+  // res.end("Welcome to basic todo app server");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All ToDos");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("ToDo Created");
+  } else {
+    res.end("404------>Route Not Found!!!!!!!!");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
